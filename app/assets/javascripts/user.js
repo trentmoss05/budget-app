8f90fc7d9a493001c3674cce6516b26092eac0b8
trentmoss05 <trentmoss05@gmail.com>
2019-05-13 13:36:43 -0400

    added brief info for events on home page

---
+++ app/assets/javascripts/user.js
@@ -0,0 +1,37 @@
+$( document ).on('turbolinks:load', function() {
+   $(".eventBrief").on("click", function(e) {
+     e.preventDefault();
+     url = this.href
+     $.ajax({
+       method: "GET",
+       url: url,
+       dataType: "json",
+       success: function (response) {
+         let expenses = response["expenses"].map(expense =>
+           "<tr> <td>"
+           + expense["name"] +
+           "</td> <td>"
+           + expense["cost"] +
+           "</td><td>"
+           + expense["quantity"] +
+           "</td>  <tr>"
+         )
+
+         let guests = "<br><h4>Guest List</h4><p><ul>" + response["guests"].map(guest =>"<li>"+guest["name"]+"</li>").join('') + "</ul></p>"
+
+          $event =$("#briefedEvent"+response['id'])
+          $guest =$("#briefedGuest"+response['id'])
+
+          $event.empty()
+          $guest.empty()
+
+          $guest.append(guests)
+          $guest.attr('class','data_block')
+          $event.append( "<table><tr><th>Name</th><th>Cost</th><th>Quantity</th></tr>"+ expenses.join('') +"</table>")
+          $event.attr('class','data_block')
+
+
+       }
+     })
+   })
+  });
